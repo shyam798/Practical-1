f316d087c718cc9113cd4041b17b593544a3ace6
wein0034 <100692220+wein0034@users.noreply.github.com>
2022-03-03 14:48:50 +1030

    Added files via upload
    Created repository and manually uploaded files from Uni PC.

---
+++ src/practical01/SemanticError.java
@@ -0,0 +1,20 @@
+package practical01;
+//*******************************************************************
+//   SemanticError.java   (Java Application)
+//   Classes:  SemanticError
+//*******************************************************************
+
+class SemanticError
+{
+    public static void main(String[] args)
+    {
+        //       System.out.println("6 plus 4 equals " + 10 + ",");
+        //       System.out.println("4 - 3 equals 1.");
+        //       System.out.println("Well" + " done!");
+        System.out.println(6 + " plus 4 equals 10,");
+        System.out.println("4 - " + 3 + " equals 1.");
+        System.out.println("Well" + " done!");
+    } // method main
+}  // class SemanticError
+
+
